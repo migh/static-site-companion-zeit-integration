eabb0b666cf2224ab5c7348c679bba3b9c0c4114
sorodrigo <rodrigo.solis@vizzuality.com>
2019-06-02 22:25:55 +0200

    remove not found res

--- src/api/webhook.route.js
+++ src/api/webhook.route.js
@@ -4,71 +4,52 @@ const fetch = require('node-fetch');
 const { getIntegrationConfig } = require('../services/mongo-integration-config');
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Authorization, Accept, Content-Type'
-  );
+  const { query } = parse(req.url, true);
+  const { config_id, owner_id } = query;
+  let integrationConfig, meta;
 
-  if (req.method === 'OPTIONS') {
-    return send(res, 200);
+  try {
+    integrationConfig = await getIntegrationConfig(owner_id);
+  } catch (e) {
+    return send(res, 500, `${e.message} – ${req.url}`);
   }
 
-  if (req.method === 'POST') {
-    const { query } = parse(req.url, true);
-    const { config_id, owner_id } = query;
-    let integrationConfig, meta;
+  const headers = {
+    Authorization: `Bearer ${integrationConfig.zeitToken}`
+  };
+  const changes = await json(req);
 
-    try {
-      integrationConfig = await getIntegrationConfig(owner_id);
-    } catch (e) {
-      return send(res, 500, `${e.message} – ${req.url}`);
-    }
-
-    const headers = {
-      Authorization: `Bearer ${integrationConfig.zeitToken}`
-    };
-    const changes = await json(req);
-
-    try {
-      meta = await fetch(`https://api.zeit.co/v1/integrations/configuration/${config_id}/metadata`, {
-        headers
-      })
-        .then(res => res.json());
-    } catch (e) {
-      return send(res, 500, `${e.message} – fetch meta`);
-    }
-
-    const newEntry = changes && {
-      version: changes.sys.version,
-      updatedAt: changes.sys.updatedAt,
-      publishedAt: changes.sys.publishedAt,
-      updatedBy: changes.sys.updatedBy && changes.sys.updatedBy.sys.id,
-      publishedVersion: changes.sys.publishedVersion
-    };
-
-    const newMeta = {
-      ...meta,
-      contentful: changes && (Array.isArray(meta.contentful) ? [newEntry, ...meta.contentful] : [newEntry])
-    };
-
-    try {
-      await fetch(`https://api.zeit.co/v1/integrations/configuration/${config_id}/metadata`, {
-        headers,
-        method: 'POST',
-        body: JSON.stringify(newMeta)
-      });
-    } catch (e) {
-      return send(res, 500, `${e.message} – post meta`);
-    }
-
-    return send(res, 200, newMeta);
+  try {
+    meta = await fetch(`https://api.zeit.co/v1/integrations/configuration/${config_id}/metadata`, {
+      headers
+    })
+      .then(res => res.json());
+  } catch (e) {
+    return send(res, 500, `${e.message} – fetch meta`);
   }
 
-  return send(res, 404, {
-    error: {
-      message: 'not_found'
-    }
-  });
+  const newEntry = changes && {
+    version: changes.sys.version,
+    updatedAt: changes.sys.updatedAt,
+    publishedAt: changes.sys.publishedAt,
+    updatedBy: changes.sys.updatedBy && changes.sys.updatedBy.sys.id,
+    publishedVersion: changes.sys.publishedVersion
+  };
+
+  const newMeta = {
+    ...meta,
+    contentful: changes && (Array.isArray(meta.contentful) ? [newEntry, ...meta.contentful] : [newEntry])
+  };
+
+  try {
+    await fetch(`https://api.zeit.co/v1/integrations/configuration/${config_id}/metadata`, {
+      headers,
+      method: 'POST',
+      body: JSON.stringify(newMeta)
+    });
+  } catch (e) {
+    return send(res, 500, `${e.message} – post meta`);
+  }
+
+  return send(res, 200, newMeta);
 };
